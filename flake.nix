ab6f932dd29618a5052370acbe179bc0d07b9a9b
Johannes Horner <j@johanneshorner.com>
2025-09-16 22:41:28 +0200

    steel option in flake

--- flake.nix
+++ flake.nix
@@ -9,68 +9,92 @@
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    rust-overlay,
-    ...
-  }: let
-    inherit (nixpkgs) lib;
-    systems = [
-      "x86_64-linux"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
-    ];
-    eachSystem = lib.genAttrs systems;
-    pkgsFor = eachSystem (system:
-      import nixpkgs {
-        localSystem.system = system;
-        overlays = [(import rust-overlay) self.overlays.helix];
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      ...
+    }:
+    let
+      inherit (nixpkgs) lib;
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      eachSystem = lib.genAttrs systems;
+      pkgsFor = eachSystem (
+        system:
+        import nixpkgs {
+          localSystem.system = system;
+          overlays = [
+            (import rust-overlay)
+            self.overlays.helix
+          ];
+        }
+      );
+      gitRev = self.rev or self.dirtyRev or null;
+    in
+    {
+      packages = eachSystem (system: {
+        inherit (pkgsFor.${system}) helix;
+        /*
+          The default Helix build. Uses the latest stable Rust toolchain, and unstable
+          nixpkgs.
+
+          The build inputs can be overridden with the following:
+
+          packages.${system}.default.override { rustPlatform = newPlatform; };
+
+          Overriding a derivation attribute can be done as well:
+
+          packages.${system}.default.overrideAttrs { buildType = "debug"; };
+        */
+        default = self.packages.${system}.helix;
+        helix-steel =
+          let
+            pkgs = pkgsFor.${system};
+            msrvToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+            rustPlatform = pkgs.makeRustPlatform {
+              cargo = msrvToolchain;
+              rustc = msrvToolchain;
+            };
+          in
+          self.packages.${system}.helix.override {
+            inherit rustPlatform;
+            enableSteel = true;
+          };
       });
-    gitRev = self.rev or self.dirtyRev or null;
-  in {
-    packages = eachSystem (system: {
-      inherit (pkgsFor.${system}) helix;
-      /*
-      The default Helix build. Uses the latest stable Rust toolchain, and unstable
-      nixpkgs.
-
-      The build inputs can be overridden with the following:
-
-      packages.${system}.default.override { rustPlatform = newPlatform; };
-
-      Overriding a derivation attribute can be done as well:
-
-      packages.${system}.default.overrideAttrs { buildType = "debug"; };
-      */
-      default = self.packages.${system}.helix;
-    });
-    checks =
-      lib.mapAttrs (system: pkgs: let
-        # Get Helix's MSRV toolchain to build with by default.
-        msrvToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-        msrvPlatform = pkgs.makeRustPlatform {
-          cargo = msrvToolchain;
-          rustc = msrvToolchain;
-        };
-      in {
-        helix = self.packages.${system}.helix.override {
-          rustPlatform = msrvPlatform;
-        };
-      })
-      pkgsFor;
-
-    # Devshell behavior is preserved.
-    devShells =
-      lib.mapAttrs (system: pkgs: {
-        default = let
-          commonRustFlagsEnv = "-C link-arg=-fuse-ld=lld -C target-cpu=native --cfg tokio_unstable";
-          platformRustFlagsEnv = lib.optionalString pkgs.stdenv.isLinux "-Clink-arg=-Wl,--no-rosegment";
+      checks = lib.mapAttrs (
+        system: pkgs:
+        let
+          # Get Helix's MSRV toolchain to build with by default.
+          msrvToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+          msrvPlatform = pkgs.makeRustPlatform {
+            cargo = msrvToolchain;
+            rustc = msrvToolchain;
+          };
         in
+        {
+          helix = self.packages.${system}.helix.override {
+            rustPlatform = msrvPlatform;
+          };
+        }
+      ) pkgsFor;
+
+      # Devshell behavior is preserved.
+      devShells = lib.mapAttrs (system: pkgs: {
+        default =
+          let
+            commonRustFlagsEnv = "-C link-arg=-fuse-ld=lld -C target-cpu=native --cfg tokio_unstable";
+            platformRustFlagsEnv = lib.optionalString pkgs.stdenv.isLinux "-Clink-arg=-Wl,--no-rosegment";
+          in
           pkgs.mkShell {
-            inputsFrom = [self.checks.${system}.helix];
-            nativeBuildInputs = with pkgs;
+            inputsFrom = [ self.checks.${system}.helix ];
+            nativeBuildInputs =
+              with pkgs;
               [
                 lld
                 cargo-flamegraph
@@ -84,15 +108,14 @@
               export RUSTFLAGS="''${RUSTFLAGS:-""} ${commonRustFlagsEnv} ${platformRustFlagsEnv}"
             '';
           };
-      })
-      pkgsFor;
+      }) pkgsFor;
 
-    overlays = {
-      helix = final: prev: {
-        helix = final.callPackage ./default.nix {inherit gitRev;};
+      overlays = {
+        helix = final: prev: {
+          helix = final.callPackage ./default.nix { inherit gitRev; };
+        };
+
+        default = self.overlays.helix;
       };
-
-      default = self.overlays.helix;
     };
-  };
 }
